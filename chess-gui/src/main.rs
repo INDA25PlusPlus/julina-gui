fcb827fad56d583f17a4cd78b0ceca0485a604f1
JulinaSavela <julina.savela@outlook.com>
2025-09-22 12:12:45 +0200

    chore: add dependencies

--- chess-gui/src/main.rs
+++ chess-gui/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use leben_chess::board::Board;
+use leben_chess::board::board_pos::BoardPosition;
+use leben_chess::board::piece::PlayerColor;
+use leben_chess::chess::{ChessError, ChessGame};
+use leben_chess::moves::{ChessMove, PieceMovement};
+
+fn main() -> Result<(), ChessError> {
+    let mut game = ChessGame::new(Board::default_board());
+    game.do_move(ChessMove {
+        piece_movement: PieceMovement {
+            from: BoardPosition::try_from("d2").unwrap(),
+            to: BoardPosition::try_from("d4").unwrap()
+        },
+        promotion: None,
+    })?;
+
+    println!("{}", game.game_status());
+    println!("{}", game.board());
+
+    let bitmap = game.available_moves(BoardPosition::try_from("d7").unwrap());
+    println!("{}", bitmap);
+
+    game.resign()?;
+
+    Ok(())
 }
